acadb9b0c56700bfd68366b21cdcc1ab446b08e3
Brian T. <bptiburcio@gmail.com>
2024-07-12 14:11:50 -0400

    Adds hello world message to homepage

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -38,5 +38,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 });
