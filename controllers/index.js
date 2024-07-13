45ecda6b8ce1b9b4b84b277f3539821fcc84a16c
Brian T. <bptiburcio@gmail.com>
2024-07-13 08:09:38 -0400

    Adds initial routings to pages

--- controllers/index.js
+++ controllers/index.js
@@ -5,4 +5,29 @@ router.get('/', (req, res) => {
     res.render('homepage');
 });
 
+router.get('/applications', (req, res) => {
+    // TODO: Check that the user is logged in here!
+    res.render('application');
+});
+
+router.get('/dashboard', (req, res) => {
+    // TODO: Check that the user is logged in here!
+    res.render('dashboard');
+});
+
+router.get('/interviews', (req, res) => {
+    // TODO: Check that the user is logged in here!
+    res.render('interviews');
+});
+
+router.get('/login', (req, res) => {
+    // TODO: Check that the user is NOT logged in here!
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    // TODO: Check that the user is NOT logged in here!
+    res.render('signup');
+});
+
 module.exports = router;
