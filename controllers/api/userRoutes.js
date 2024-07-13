913009583e8a3747185436482e303751e2b72393
Megan McNeill <mcneim2@gmail.com>
2024-07-13 18:31:07 -0400

    added login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -25,6 +25,46 @@ router.post('/api', async (req,res) => {
 });
 
 // login
+router.post('/login', async (req,res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+        // check if the user (email) exists in the database
+        if (!userData) {
+            res
+            .status(400)
+            .json({ message: 'No User with that email. Please try again'});
+            
+            return;
+        }
+        // set the variable validPassword to equal the output of the method checkPassword - agaignst the request body password
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+            .status(400)
+            .json({ message: 'Incorrect email or password. Please try again'})
+
+            return;
+        }
+
+        // if a valid email and password have been input, set the session variable to loggedIn
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res
+                .status(200)
+                .json({ user: userData, message: 'You are now logged in'});
+        });
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);        
+    }
+})
 
 // logout
 
