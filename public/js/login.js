0a08023456ace49c69c5718bd55def83ce1809b5
Brian T. <bptiburcio@gmail.com>
2024-07-13 21:03:49 -0400

    Adds javascript for login

---
+++ public/js/login.js
@@ -0,0 +1,59 @@
+const loginForm = document.querySelector('#login-form');
+const signUpForm = document.querySelector('#signup-form');
+
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const login = document.querySelector('#loginInput').value.trim();
+    const password = document.querySelector('#loginPasswordInput').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ login, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in.');
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#signUpEmailInput').value.trim();
+
+    const username = document
+        .querySelector('#signUpUsernameInput')
+        .value.trim();
+
+    const password = document
+        .querySelector('#signUpPasswordInput')
+        .value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/signup', {
+            method: 'POST',
+            body: JSON.stringify({ email, username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to sign up.');
+        }
+    }
+};
+
+if (loginForm) {
+    loginForm.addEventListener('submit', loginFormHandler);
+}
+
+if (signUpForm) {
+    signUpForm.addEventListener('submit', signupFormHandler);
+}
