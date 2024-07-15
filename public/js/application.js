4b42a293e271e4cab400cb77ae65ca26bff6945b
Laura <laurawatersalston@gmail.com>
2024-07-15 13:41:23 -0400

    adds handlebars to dashboard.hbs

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,1 @@
-const statusDropdown = document.querySelector('#statusDropdown');
-
-
-document.getElementById('statusDropdown').addEventListener('change', function() {
-    const selectedStatus = this.value;
-    Application.forEach(app => {
-        if (app.status === selectedStatus) {
-            app.status = selectedStatus;
-        }
-    });
-    const updatedHtml = template({ Application });
-    document.getElementById('tableContainer').innerHTML = updatedHtml;
-});   
+  
