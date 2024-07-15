770189ae98d3c11fd26f60ea4d050f2ea55c9853
Alctoria <defruits2006@gmail.com>
2024-07-16 03:27:56 +0900

    uniform calendar
    created a uniform style for id calendar under styles.css,

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const path = require('path');
 
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const hbs = exphbs.create({ extname: '.hbs' });
