783ca80ff01ff18f79bc2e43a2b7ee79c07e337e
Brian T. <bptiburcio@gmail.com>
2024-07-15 12:34:48 -0400

    Adds seeding for interviews table

--- models/index.js
+++ models/index.js
@@ -1,12 +1,21 @@
 // Import models
 const User = require('./User');
+const Interview = require('./Interview');
 const { Application } = require('./Application');
 
-// Define relationships
-
+// Define the relationship between the a user and an application
 User.hasMany(Application);
+Application.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
 
-Application.belongsTo(User);
+// Define the relationship between an application and an interview
+Interview.belongsTo(Application, {
+    foreignKey: 'application_id',
+    onDelete: 'CASCADE',
+});
+Application.hasMany(Interview);
 
 // Export modules
-module.exports = { User, Application };
+module.exports = { User, Application, Interview };
