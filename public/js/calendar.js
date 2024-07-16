58e136b0abf995f72f1ed7453d8ce810eee39d4b
Alctoria <defruits2006@gmail.com>
2024-07-17 03:26:40 +0900

    calendar.js seed insertion code
    when button is submitted, information from the input is transported into the seed

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
   
@@ -30,3 +31,62 @@ document.addEventListener('DOMContentLoaded', function() {
   
     calendar.render();
   });
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    initialDate: '2024-07-07',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    events: [
+      {
+          title: 'Instagram Interview',
+          url: 'https://instagram.com/',
+          start: '2024-07-17T19:00:00'
+      },
+      {
+          title: 'Github Interview',
+          url: 'https://github.com/',
+          start: '2024-07-20T13:00:00'
+      },
+      {
+          title: 'Google Interview',
+          url: 'https://google.com/',
+          start: '2024-07-25T07:00:00'
+      }
+    ]
+  });
+
+  calendar.render();
+
+  document.getElementById('submitEventBtn').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    var dateInput = document.getElementById('interviewDateInput').value;
+    var timeInput = document.getElementById('interviewTimeInput').value;
+    var videoLinkInput = document.getElementById('interviewVideoLinkInput').value;
+
+    var startDateTime = dateInput + 'T' + timeInput;
+
+    if (dateInput && timeInput && videoLinkInput) {
+      calendar.addEvent({
+        title: 'Custom Interview',
+        start: startDateTime,
+        url: videoLinkInput
+      });
+
+      document.getElementById('interviewDateInput').value = '';
+      document.getElementById('interviewTimeInput').value = '';
+      document.getElementById('interviewVideoLinkInput').value = '';
+
+      calendar.render();
+    } else {
+      alert('Please fill out all fields.');
+    }
+  });
+});
