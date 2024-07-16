d0f8850f7b2af9b7e020584f36b420c08fc506b2
Laura <laurawatersalston@gmail.com>
2024-07-16 13:11:17 -0400

    adds displayStatus helper function

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,20 +1,46 @@
 module.exports = {
+    // function to display status in badge  
+    displayStatus: (status) => {
+        switch (status) {
+            case 1:
+                return 'Applied';
+            case 2:
+                return 'Interview Scheduled';
+            case 3:
+                return 'Interviewed';
+            case 4:
+                return 'On Hold';
+            case 5:
+                return 'Offer Received';
+            case 6:
+                return 'Offer Accepted';
+            case 7:
+                return 'Offer Rejected';
+            case 8:
+                return 'Application Rejected';
+            default:
+                return 'Applied';
+        }
+    },
+    
     // function to assign each status a badge class 
     getStatusBadgeClass: (status) => {
         switch (status) {
-            case 'Applied':
+            case 1:
                 return 'badge-primary';
-            case 'Interview-Scheduled':
+            case 2:
                 return 'badge-info';
-            case 'Interviewed':
+            case 3:
                 return 'badge-warning';
-            case 'On-Hold':
+            case 4:
                 return 'badge-dark';
-            case 'Offer-Received':
+            case 5:
                 return 'badge-success';
-            case 'Offer-Accepted':
+            case 6:
                 return 'badge-success';
-            case 'Application-Rejected':
+            case 7:
+                return 'badge-dark';
+            case 8:
                 return 'badge-danger';
             default:
                 return 'badge-primary';
